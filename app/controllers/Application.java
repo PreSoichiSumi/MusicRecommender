713d8cd83afb0c644c3a91c930011f977992ea51
SoichiSumi <s-sumi@ist.osaka-u.ac.jp>
2016-02-23 00:06:29 +0900

    メッセージ変更のテスト

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,12 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Application extends Controller {
 
     public Result index() {
-        return ok("This is MusicRecommender.");
+        return ok("This message is modified.");
     }
 
 }
